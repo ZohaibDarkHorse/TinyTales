982590ba77eb561214dace97355b3b3977ed9c44
ZohaibDarkHorse <128962091+ZohaibDarkHorse@users.noreply.github.com>
2024-10-28 12:03:14 +0530

    Update TinyTalesClientController.java

--- TurfProjectAPIs/src/main/java/com/tinyTales/project/TinyTalesProjectAPIs/controller/TinyTalesClientController.java
+++ TurfProjectAPIs/src/main/java/com/tinyTales/project/TinyTalesProjectAPIs/controller/TinyTalesClientController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tinyTales.project.TinyTalesProjectAPIs.model.RegisterTinyTalesClient;
 import com.tinyTales.project.TinyTalesProjectAPIs.service.TinyTalesClientService;
-@CrossOrigin(origins = "http://localhost:5173/")
+@CrossOrigin(origins = "https://tiny-tales-main-zo-test-git.apps.nprdc-ocp.dhdigital.co.in")
 @RestController
 public class TinyTalesClientController {
 
