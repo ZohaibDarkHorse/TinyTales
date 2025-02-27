aa41d08e9e2ace2e4a0f439d6023bd56b94bffe3
ZohaibDarkHorse <128962091+ZohaibDarkHorse@users.noreply.github.com>
2025-02-27 17:40:44 +0530

    Update WebConfig.java

--- TurfProjectAPIs/src/main/java/com/tinyTales/project/TinyTalesProjectAPIs/config/WebConfig.java
+++ TurfProjectAPIs/src/main/java/com/tinyTales/project/TinyTalesProjectAPIs/config/WebConfig.java
@@ -1,15 +1,11 @@
-package com.tinyTales.project.TinyTalesProjectAPIs.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-
+        registry.addMapping("/**")
+                .allowedOrigins("*")  // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")  // Allow all headers
+                .allowCredentials(true);
     }
 }
