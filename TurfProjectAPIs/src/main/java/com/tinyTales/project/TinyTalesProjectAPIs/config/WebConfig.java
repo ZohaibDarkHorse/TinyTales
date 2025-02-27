5a6e0bdb033205c55f9cca5403edb7b07c8cccc3
ZohaibDarkHorse <128962091+ZohaibDarkHorse@users.noreply.github.com>
2025-02-27 17:49:15 +0530

    Update WebConfig.java

--- TurfProjectAPIs/src/main/java/com/tinyTales/project/TinyTalesProjectAPIs/config/WebConfig.java
+++ TurfProjectAPIs/src/main/java/com/tinyTales/project/TinyTalesProjectAPIs/config/WebConfig.java
@@ -3,9 +3,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")  // Allow all origins
+                .allowedOrigins("*")  
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")  // Allow all headers
+                .allowedHeaders("*")  
                 .allowCredentials(true);
     }
 }
