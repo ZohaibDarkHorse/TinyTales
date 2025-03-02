93aa88ae8d5efdaa827beff431e52a6e9d666eb4
ZohaibDarkHorse <128962091+ZohaibDarkHorse@users.noreply.github.com>
2025-03-02 22:11:01 +0530

    Update WebConfig.java

--- TurfProjectAPIs/src/main/java/com/tinyTales/project/TinyTalesProjectAPIs/config/WebConfig.java
+++ TurfProjectAPIs/src/main/java/com/tinyTales/project/TinyTalesProjectAPIs/config/WebConfig.java
@@ -1,25 +1,25 @@
 package com.tinyTales.project.TinyTalesProjectAPIs.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
-public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://tiny-tales-git-12-zo-test-git.apps.nprdc-ocp.dhdigital.co.in")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true); // Important for authentication
-            }
-        };
-    }
-}
+// @Configuration
+// @EnableWebMvc
+// public class WebConfig {
+//     @Bean
+//     public WebMvcConfigurer corsConfigurer() {
+//         return new WebMvcConfigurer() {
+//             @Override
+//             public void addCorsMappings(CorsRegistry registry) {
+//                 registry.addMapping("/**")
+//                         .allowedOrigins("https://tiny-tales-git-12-zo-test-git.apps.nprdc-ocp.dhdigital.co.in")
+//                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                         .allowedHeaders("*")
+//                         .allowCredentials(true); // Important for authentication
+//             }
+//         };
+//     }
+// }
