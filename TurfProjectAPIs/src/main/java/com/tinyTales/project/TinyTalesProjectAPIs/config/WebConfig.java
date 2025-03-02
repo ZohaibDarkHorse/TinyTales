75b323bd6a9d1c315e4a529e09c04329238d7738
ZohaibDarkHorse <128962091+ZohaibDarkHorse@users.noreply.github.com>
2025-03-02 15:56:13 +0530

    Update WebConfig.java

--- TurfProjectAPIs/src/main/java/com/tinyTales/project/TinyTalesProjectAPIs/config/WebConfig.java
+++ TurfProjectAPIs/src/main/java/com/tinyTales/project/TinyTalesProjectAPIs/config/WebConfig.java
@@ -1,3 +1,5 @@
+package com.tinyTales.project.TinyTalesProjectAPIs.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -6,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class CorsConfig {
+public class WebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
